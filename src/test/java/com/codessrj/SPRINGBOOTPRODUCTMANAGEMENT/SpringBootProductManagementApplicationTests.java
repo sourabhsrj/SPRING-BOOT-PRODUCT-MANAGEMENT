0272b69e33859478addf224559a5cad7ab22107e
Sourabhjadhav1 <sourabh.jadhav@enzegar.con>
2023-06-26 11:25:51 +0530

    Initial commit. created spring boot project and added required depedencies.

---
+++ src/test/java/com/codessrj/SPRINGBOOTPRODUCTMANAGEMENT/SpringBootProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codessrj.SPRINGBOOTPRODUCTMANAGEMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
